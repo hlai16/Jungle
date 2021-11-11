e9841e1039cd4aa9ed3967e3c741ff6e257c8d72
hlai16 <gloriajoanlai@gmail.com>
2021-11-10 20:30:26 -0500

    Dashboard display with product and category counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,10 +1,12 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV["ADMIN_USERNAME"].to_s, password: ENV["ADMIN_PASSWORD"].to_s
+  http_basic_authenticate_with name: ENV["ADMIN_USER"], password: ENV["ADMIN_PASSWORD"]
   def index
   end
   def edit
     render plain: "I'm only accessible if you know the password"
   end
   def show
+    @total_products = Product.count
+    @total_categories = Category.count
   end
 end
