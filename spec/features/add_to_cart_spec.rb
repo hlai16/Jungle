37527724109c92e805caa7d1f60b0312da1d67f8
hlai16 <gloriajoanlai@gmail.com>
2021-11-16 21:43:59 -0500

    add to cart spec completion by myself

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature, js: true do
+RSpec.feature "AddToCarts", type: :feature, js: true do
   before :each do
     @category = Category.create! name: 'Apparel'
 
@@ -14,18 +14,11 @@ RSpec.feature "ProductDetails", type: :feature, js: true do
       )
     end
   end
-  
-  scenario "Product detail shows when clicked on Details link" do
+  scenario "Click 'Add' button to increase the count in cart" do
     visit root_path
     product1 = page.all('.product')[0]
-    product1.click_on 'Details'
-    expect(page).to have_css '.products-show'
-    save_and_open_screenshot
-
-    visit root_path
-    product1 = page.all('.product header a')[0]
-    product1.click
-    expect(page).to have_css '.products-show'
+    product1.click_button 'Add'
+    within('nav') { expect(page).to have_content('My Cart', count: 1) }
     save_and_open_screenshot
   end
 end
